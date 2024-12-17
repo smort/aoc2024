9750f55dc644e330e4c0be3b0d5da5a6f686b992
smort <samaccounts@outlook.com>
2024-12-17 16:39:11 -0700

    add template

---
+++ go/template/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	part1("example")
+}
+
+func part1(filename string) {
+	result := 0
+
+	fmt.Println(result)
+}
+
+func part2(filename string) {
+	result := 0
+
+	fmt.Println(result)
+}
